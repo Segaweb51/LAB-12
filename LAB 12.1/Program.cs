bf160856f1af12fac5322850e84a7e1054d1e49b
Segaweb51 <sergeysesd@yandex.ry>
2022-02-14 23:04:16 +0300

    Добавьте файлы проекта.

---
+++ LAB 12.1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_12._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.Write("Введите радиус :");
+            Double Radius = Convert.ToDouble(Console.ReadLine());
+            if (Radius <= 0)
+            {
+                Console.Write("Радиус натуральное положительное число");
+                return;
+            }
+            Console.Write("Введите координату X");
+            double positionX = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Введите координату Y");
+            double positionY = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+
+            Circle.GetСircleline(Radius);
+            Circle.GetSquare(Radius);
+            Circle.GetPosition(Radius, positionX, positionY);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный радиус");
+                return;
+            }
+        }
+        static class Circle
+        {
+            public static void GetСircleline(double Radius)
+            {
+                double Сircumference = Math.PI * 2 * Radius;
+                Console.WriteLine("Длина окружности равна {0:f2}", Сircumference);
+                
+            }
+            public static void GetSquare(double Radius)
+            {
+                double Square = Math.PI * Math.Pow(Radius, 2.0);
+                Console.WriteLine("Площадь круга равна {0:f2}", Square);
+                
+
+            }
+            public static void GetPosition(double Radius, double positionX, double positionY)
+            {
+                if (positionX <= Radius && positionY <= Radius)
+                {
+                    Console.WriteLine("Точка с координатами ({0},{1}) принадлежит кругу с радиусом {2}", positionX, positionY, Radius);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Точка с координатами ({0},{1}) не принадлежит кругу с радиусом {2}", positionX, positionY, Radius);
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
---
+++ LAB 12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cat.voice);
+            Console.ReadKey();
+        }
+        public class Cat
+        {
+            public static string voice = "Мяу";
+            public const int countFeet = 4;
+            public readonly bool wool = true;
+            public string Name { get; set; }
+            public string Color { get; set; }
+            int age;
+
+            public int Age;
+
+            public Cat (string name, string colore, int age, bool wool)
+            {
+                Name = name;
+                Color = Color;
+                Age = age;
+                this.wool = wool;
+            }
+            public void Say()
+            {
+                Console.WriteLine("Мяу. Меня зовут {0}. У меня {1} цвет. Мне {2} лет.", Name, Color, Age);
+            }
+     
+        }
+    }
+}
